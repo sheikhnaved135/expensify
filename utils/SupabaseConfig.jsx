598018836d1b56fd9dbac9ee93df04189e6fb655
sheikhnaved <75406847+sheikhnaved135@users.noreply.github.com>
2024-11-02 20:25:03 +0530

    Update SupabaseConfig.jsx

--- utils/SupabaseConfig.jsx
+++ utils/SupabaseConfig.jsx
@@ -1,7 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
 
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(
-  "https://atbinfbpmfypmzvrilas.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0YmluZmJwbWZ5cG16dnJpbGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwOTU0MTcsImV4cCI6MjA0NTY3MTQxN30.ZzyPajzmYJZcUHsgkar8EUNuHbVwQFnucYQzOi8GeqY"
-);
